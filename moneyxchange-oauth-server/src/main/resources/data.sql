2e36823867edf59db164fc66e73c711e3033acba
Danilo Nicolas Mendoza RIcladi <nmendozaricaldi@gmail.com>
2018-03-12 13:52:00 -0500

    money_rate values

--- moneyxchange-oauth-server/src/main/resources/data.sql
+++ moneyxchange-oauth-server/src/main/resources/data.sql
@@ -5,4 +5,6 @@ INSERT INTO users(username,password,enabled) VALUES ('user','user', true);
 INSERT INTO user_roles (username, role) VALUES ('admin', 'ROLE_USER');
 INSERT INTO user_roles (username, role) VALUES ('admin', 'ROLE_ADMIN');
 INSERT INTO user_roles (username, role) VALUES ('user', 'ROLE_USER');
+
+INSERT INTO money_rate (symbol, rate) VALUES ('EU', 1.079);
 */
--- moneyxchange-oauth-server/src/main/resources/schema.sql
+++ moneyxchange-oauth-server/src/main/resources/schema.sql
@@ -12,4 +12,12 @@ create table user_roles (
   role varchar(20) NOT NULL,
   UNIQUE (username,role),
   FOREIGN KEY (username) REFERENCES users (username)
-);*/
+);
+
+CREATE TABLE money_rate
+(
+    symbol character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    rate double precision,
+    CONSTRAINT money_rate_pkey PRIMARY KEY (symbol)
+)
+*/
